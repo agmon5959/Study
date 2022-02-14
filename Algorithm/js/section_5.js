88e357bda844a5f4340ae678ea59aaab50c9f131
김동훈A <agmon5959@douzone.com>
2022-02-14 19:54:23 +0900

    0214 commit

--- Algorithm/js/section_5.js
+++ Algorithm/js/section_5.js
@@ -2,24 +2,51 @@
 
 // 1번 문제
 // 두 배열 합치기
-function solution(arr1, arr2){
+function solution(arr1, arr2) {
     let newArr = [];
     let len = arr1.length < arr2.length ? arr2.length : arr1.length
 
     for (let i = 0; i < len; i++) {
-        if(arr1[i] !== undefined){
+        if (arr1[i] !== undefined) {
             newArr.push(arr1[i]);
         }
-        if(arr2[i] !== undefined){
+        if (arr2[i] !== undefined) {
             newArr.push(arr2[i]);
         }
     }
-    
-    return newArr.sort((a,b)=>{
-        return a-b;
+
+    return newArr.sort((a, b) => {
+        return a - b;
     })
 }
 
-let a=[1, 3, 5];
-let b=[2, 3, 6, 7, 9];
-console.log(solution(a, b));
+// 2번 문제
+// 공통원소 구하기
+function solution(arr1, arr2){
+    let ans=[];
+    arr1.forEach((iter)=>{
+        if(arr2.indexOf(iter) >= 0 ){
+            ans.push(iter)
+        }
+    })
+    return ans.sort((a,b)=>{return a-b})
+}
+
+// 3번 문제
+// 연속 부분 수열
+function solution(m, arr){
+    let ans;
+    let cnt=0;
+    let sum = 0;
+    for(let i=0;i<arr.length;i++){
+        sum += arr[i];
+        for(let j=0; j<arr.length;j++){
+            if(i === j) { break; }
+            sum += arr[j];
+        }
+    }
+    return ans;
+}
+
+let a=[1, 2, 1, 3, 1, 1, 1, 2];
+console.log(solution(6, a));
