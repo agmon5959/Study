7b956889d021841167508aeb4658534d0110faf0
김동훈A <agmon5959@douzone.com>
2022-02-15 22:55:36 +0900

    commit

--- Algorithm/js/section_5.js
+++ Algorithm/js/section_5.js
@@ -22,31 +22,55 @@ function solution(arr1, arr2) {
 
 // 2번 문제
 // 공통원소 구하기
-function solution(arr1, arr2){
-    let ans=[];
-    arr1.forEach((iter)=>{
-        if(arr2.indexOf(iter) >= 0 ){
+function solution(arr1, arr2) {
+    let ans = [];
+    arr1.forEach((iter) => {
+        if (arr2.indexOf(iter) >= 0) {
             ans.push(iter)
         }
     })
-    return ans.sort((a,b)=>{return a-b})
+    return ans.sort((a, b) => { return a - b })
 }
 
 // 3번 문제
 // 연속 부분 수열
-function solution(m, arr){
-    let ans;
-    let cnt=0;
-    let sum = 0;
-    for(let i=0;i<arr.length;i++){
-        sum += arr[i];
-        for(let j=0; j<arr.length;j++){
-            if(i === j) { break; }
-            sum += arr[j];
+function solution(m, arr) {
+    let left = 0;
+    let right = 1;
+    let cnt = 0;
+    let sum = arr[left] + arr[right];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[right] === undefined || arr[left] === undefined) break;
+        debugger
+        if (sum < m) { // sum이 m보다 작을 때
+            debugger
+            sum += arr[right];
+            right++;
+            if (sum + arr[right] === m) {
+                sum -= arr[left];
+                cnt++;
+                left--;
+            }
+        } else if (sum > m) {
+            debugger
+            sum -= arr[left];
+            left++;
+            
+            if (sum + arr[right] === m) {
+                sum -= arr[left];
+                cnt++;
+                left--;
+            }
+            
         }
+
+
     }
-    return ans;
+    debugger
+
+    return cnt;
 }
 
-let a=[1, 2, 1, 3, 1, 1, 1, 2];
+let a = [1, 2, 1, 3, 1, 1, 1, 2];
 console.log(solution(6, a));
