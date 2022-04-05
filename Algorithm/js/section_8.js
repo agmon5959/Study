fc6f99d544a616e4a3f21e79f56a5ff0620ddafd
김동훈A <agmon5959@douzone.com>
2022-04-05 23:34:53 +0900

    commit

--- Algorithm/js/section_8.js
+++ Algorithm/js/section_8.js
@@ -74,4 +74,11 @@ function solution(n) {
     return answer;
 }
 
-console.log(solution(3));
+// 5번 문제
+// 합이 같은 부분집합
+function solution(arr){
+   
+}
+
+let arr=[1, 3, 5, 6, 7, 10];
+console.log(solution(arr));
