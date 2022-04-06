b065c1e2ed4a3207f8389c7e3073cb129f65303a
김동훈A <agmon5959@douzone.com>
2022-04-06 21:39:26 +0900

    commit

--- Algorithm/js/section_8.js
+++ Algorithm/js/section_8.js
@@ -76,9 +76,22 @@ function solution(n) {
 
 // 5번 문제
 // 합이 같은 부분집합
-function solution(arr){
-   
+function solution(arr) {
+
 }
 
-let arr=[1, 3, 5, 6, 7, 10];
-console.log(solution(arr));
+
+
+function solution(new_id) {
+
+    // 1단계 new_id의 모든 대문자를 대응되는 소문자로 치환합니다.
+    let ans = new_id.toLowerCase()
+    // 2단계 new_id에서 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거합니다.
+    ans.replaceAll( )
+    debugger
+
+
+
+}
+const param = "...!@BaT#*..y.abcdefghijklm";
+console.log(solution(param))
