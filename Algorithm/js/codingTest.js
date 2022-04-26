8253b8e3ed99021fef1707d08a7a472d784f0354
김동훈A <agmon5959@douzone.com>
2022-04-26 23:34:31 +0900

    commit

--- Algorithm/js/codingTest.js
+++ Algorithm/js/codingTest.js
@@ -64,4 +64,35 @@ function solution(s) {
     }
     return Number(arr.join(""));
 }
-console.log(solution("123"));
+
+// 숫자 문자열과 영단어 깔끔한 풀이
+function solution(s) {
+    let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    var answer = s;
+
+    for(let i=0; i< numbers.length; i++) {
+        let arr = answer.split(numbers[i]);
+        answer = arr.join(i);
+    }
+
+    return Number(answer);
+}
+console.log(solution("onetwo3four56seven"));
+
+// 숫자 문자열과 영단어 
+// 정규표현식 풀이
+function solution(s) {
+
+    s = s.replace(/zero/gi, 0)
+    .replace(/one/gi, 1)
+    .replace(/two/gi, 2)
+    .replace(/three/gi, 3)
+    .replace(/four/gi, 4)
+    .replace(/five/gi, 5)
+    .replace(/six/gi, 6)
+    .replace(/seven/gi, 7)
+    .replace(/eight/gi, 8)
+    .replace(/nine/gi, 9)
+    return parseInt(s);
+    
+}
