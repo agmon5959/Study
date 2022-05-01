75d59ea4693736471dbd885d2e95eb157842bd12
김동훈A <agmon5959@douzone.com>
2022-05-01 21:09:30 +0900

    commit

--- Algorithm/js/codingTest.js
+++ Algorithm/js/codingTest.js
@@ -1,7 +1,7 @@
 // 프로그래머스 1단계 신고결과받기
 // https://programmers.co.kr/learn/courses/30/lessons/92334
 
-function solution(id_list, report, k) {
+function solution2(id_list, report, k) {
     const answer = new Array(id_list.length);
     answer.fill(0);
     const report_list = {};
@@ -10,6 +10,7 @@ function solution(id_list, report, k) {
     id_list.forEach((iter) => {
         report_list[iter] = [];
     })
+
 
     // 중복제거
     const reportArr = [...new Set(report)]
@@ -30,7 +31,7 @@ function solution(id_list, report, k) {
     }
 }
 
-// console.log(solution(["muzi", "frodo", "apeach", "neo"], ["muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"], 2));
+console.log(solution2(["muzi", "frodo", "apeach", "neo"], ["muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"], 2));
 
 // 프로그래머스 1단계
 // 숫자 문자열과 영단어
@@ -87,21 +88,8 @@ function solution(s) {
     return parseInt(s);
 
 }
-console.log(solution("onetwo3four56seven"));
+// console.log(solution("onetwo3four56seven"));
 
-
-
-
-
-
-
-
-function solution(string) {
-
-
-
-}
-console.log(solution("...!@BaT#*..y.abcdefghijklm"));
 
 // 크레인 인형뽑기
 function solution(board, moves) {
@@ -131,4 +119,48 @@ function solution(board, moves) {
     return score;
 }
 
-// 
+
+// 로또의 최고순위와 최저순위
+// 내 풀이
+function solution(lottos, win_nums) {
+    // 정답을 넣을 배열
+    const returnArr = [];
+    // 높은 점수를 담을 high 변수이며 변수 값이 바뀌기 때문에 let
+    let high = 0;
+    // 낮은 점수를 담을 low 변수이며 변수 값이 바뀌기 때문에 let
+    let low = 0;
+    // 민우의 로또 번호를 순회하여 최고순위와 최저순위를 구한다.
+    lottos.forEach((iter) => {
+        // 단어의 위치까지 일치하지 않아도 되니 그냥 존재하는지 확인
+        if (win_nums.indexOf(iter) >= 0) {
+            high++;
+            low++;
+        // 존재하지 않고 0이라면 일치한다고 판단. (high는 최고 순위이기 때문)
+        } else if (iter === 0) {
+            high++;
+        }
+    })
+    // 7-점수 하면 등수가 나오며 최저 6등이기에 점수가 0점이면 6으로 넣어주도록 한다.
+    returnArr[0] = 7 - high > 6 ? 6 : 7 - high;
+    returnArr[1] = 7 - low > 6 ? 6 : 7 - low;
+    return returnArr;
+
+
+    // answer = [ 최고순위 , 최저순위]
+}
+
+// 로또의 최고순위와 최저순위
+// 깔끔한 풀이
+function solution(lottos, win_nums) {
+    const rank = [6, 6, 5, 4, 3, 2, 1];
+
+    // 일치하는 애들의 길이
+    let minCount = lottos.filter(v => win_nums.includes(v)).length; // 2개 일치 ? > 2 return
+    // 일치하지 않는 애들의 길이
+    let zeroCount = lottos.filter(v => !v).length;
+
+    const maxCount = minCount + zeroCount;
+
+    return [rank[maxCount], rank[minCount]];
+}
+solution([44, 1, 0, 0, 31, 25],[31, 10, 45, 1, 6, 19]);
