685b8ade45e650d68763559730e36ccce3d195cb
김동훈A <agmon5959@douzone.com>
2022-05-24 14:39:46 +0900

    commit

--- Algorithm/js/codingTest.js
+++ Algorithm/js/codingTest.js
@@ -828,3 +828,21 @@ function solution(s) {
     return s.substr(Math.ceil(s.length / 2) - 1, s.length % 2 === 0 ? 2 : 1);
 }
 
+// 최소 직사각형
+function solution(sizes) {
+    let horizon = []
+    let vertical = []
+    for (let i = 0; i < sizes.length; i++) {
+
+        if (sizes[i][0] < sizes[i][1]) {
+            horizon.push(sizes[i][1]);
+            vertical.push(sizes[i][0]);
+        } else {
+            horizon.push(sizes[i][0]);
+            vertical.push(sizes[i][1]);
+        }
+    }
+    return Math.max(...horizon) * Math.max(...vertical);
+}
+
+
