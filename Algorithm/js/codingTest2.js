f6438921c4c2a6ce30f1539c173d9c76266a1d78
김동훈A <agmon5959@douzone.com>
2022-06-11 16:50:18 +0900

    commit

--- Algorithm/js/codingTest2.js
+++ Algorithm/js/codingTest2.js
@@ -14,8 +14,80 @@ function solution(s) {
             }
             i = -1;
         }
-        
+
 
     }
+    return ans;
+}
+
+function solution(s) {
+    let ans = 0;
+    let arr = [...s];
+
+    for (let i = 0; i < s.length; i++) {
+
+        if (arr[i] === arr[i + 1]) {
+            arr = arr.slice(0, i).concat(arr.slice(i + 2));
+            i = i - 2;
+        }
+
+        if (arr.length === 2) {
+            if (arr[0] === arr[1]) {
+                return 1;
+            }
+        }
+
+    }
+    return ans;
+}
+
+function solution(s) {
+    let compArr = [];
+    let p = -1;
+    let ans = 0;
+
+
+    for (let i = 0; i < s.length; i++) {
+        // 두 개가 다른 경우
+        if (s[i] !== compArr[p]) {
+            compArr.push(s[i]);
+            p++;
+            // 두 개가 같은 경우
+        } else {
+            compArr.pop();
+            p--;
+        }
+    }
+
+    if (compArr.length == 0) {
+        ans = 1;
+    }
+    return ans;
+
+}
+
+// 최종 성공한 풀이
+function solution(s) {
+    const compArr = [];
+    let p = -1;
+    let ans = 0;
+
+
+    for (let i = 0; i < s.length; i++) {
+        // 두 개가 다른 경우
+        if (s[i] !== compArr[p]) {
+            compArr.push(s[i]);
+            p++;
+            // 두 개가 같은 경우
+        } else {
+            compArr.pop();
+            p--;
+        }
+    }
+
+    if (compArr.length == 0) {
+        return 1;
+    }
+
     return ans;
 }
