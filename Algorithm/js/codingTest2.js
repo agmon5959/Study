372d5c4fbe44bd4e3d2ee3886c195e3839acaba9
김동훈A <agmon5959@douzone.com>
2022-06-13 16:15:01 +0900

    commit

--- Algorithm/js/codingTest2.js
+++ Algorithm/js/codingTest2.js
@@ -131,3 +131,16 @@ function solution(rows, columns, queries) {
 
     return answer;
 }
+
+// 124 숫자의 나라
+function solution(n) {
+    var answer = '';
+    const arr = ['4', '1', '2'];
+    while (n > 0) {
+        answer = arr[n % 3] + answer;
+        n = Math.floor((n - 1) / 3);
+    }
+
+
+    return answer;
+}
