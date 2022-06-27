79f7ccbf682d76e9014dfd992c6ff75485bec79c
김동훈A <agmon5959@douzone.com>
2022-06-28 01:33:01 +0900

    백준초기세팅

---
+++ BOJ/index.js
@@ -0,0 +1,11 @@
+// 띄어쓰기로 들어오는 경우
+// let input = require('fs').readFileSync('input.txt').toString().split('\n');
+// var a = parseInt(input[0]);
+// var b = parseInt(input[1]);
+// console.log(a + b);
+
+// 줄바꿈으로 들어오는 경우
+let input = require('fs').readFileSync('input.txt').toString().split('\n');
+var a = parseInt(input[0]);
+var b = parseInt(input[1]);
+console.log(a, b);
