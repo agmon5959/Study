b1a71b4292846f66986e06dc07f08ae219c49b91
김동훈A <agmon5959@douzone.com>
2022-06-28 01:41:14 +0900

    백준 테스트 커밋 완료

---
+++ BOJ/code/10926.js
@@ -0,0 +1,5 @@
+// 채점용 /dev/stdin
+let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+var a = (input[0]);
+// var b = parseInt(input[1]);
+console.log((a) + '??!');
--- BOJ/index.js
+++ BOJ/index.js
@@ -1,11 +1,12 @@
 // 띄어쓰기로 들어오는 경우
+// let input = require('fs').readFileSync('input.txt').toString().split(' ');
+// 줄바꿈으로 들어오는 경우
 // let input = require('fs').readFileSync('input.txt').toString().split('\n');
 // var a = parseInt(input[0]);
 // var b = parseInt(input[1]);
-// console.log(a + b);
 
 // 줄바꿈으로 들어오는 경우
 let input = require('fs').readFileSync('input.txt').toString().split('\n');
-var a = parseInt(input[0]);
-var b = parseInt(input[1]);
-console.log(a, b);
+var a = (input[0]);
+// var b = parseInt(input[1]);
+console.log((a) + '??!');
