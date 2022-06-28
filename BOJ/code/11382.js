31161ca84d6a9c978bb1d8aef5c277fcbe865f4b
김동훈A <agmon5959@douzone.com>
2022-06-28 20:28:08 +0900

    BOJ -1-

---
+++ BOJ/code/11382.js
@@ -0,0 +1,5 @@
+let input = require('fs').readFileSync('input.txt').toString().split(' ');
+let a = parseInt(input[0]);
+let b = parseInt(input[1]);
+let c = parseInt(input[2]);
+console.log(a + b + c);
