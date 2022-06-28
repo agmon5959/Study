64acaaffded2de445b1d3d56d3e29b8cac2c013a
김동훈A <agmon5959@douzone.com>
2022-06-28 20:09:44 +0900

    백준 -1-

--- BOJ/index.js
+++ BOJ/index.js
@@ -5,8 +5,6 @@
 // var a = parseInt(input[0]);
 // var b = parseInt(input[1]);
 
-// 줄바꿈으로 들어오는 경우
 let input = require('fs').readFileSync('input.txt').toString().split('\n');
-var a = (input[0]);
-// var b = parseInt(input[1]);
-console.log((a) + '??!');
+var a = parseInt(input[0]);
+var b = parseInt(input[1]);
