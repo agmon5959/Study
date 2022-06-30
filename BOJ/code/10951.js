5efd40b00ccc5ebdba6d66ca41f57685e816b2c1
김동훈A <agmon5959@douzone.com>
2022-06-30 20:47:01 +0900

    commit

--- BOJ/code/10951.js
+++ BOJ/code/10951.js
@@ -1,9 +1,7 @@
 // A+B
-
-
 let input = require('fs').readFileSync('input.txt').toString().trim().split('\n');
 let ansArr = input.map(iter => iter.split(' '));
 
-for (let i = 1; i < ansArr.length; i++) {
+for (let i = 0; i < ansArr.length; i++) {
     console.log(Number(ansArr[i][0]) + Number(ansArr[i][1]));
 }
