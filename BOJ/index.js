1c89c9c03fcec7ce409a1d1712947267665fa360
김동훈A <agmon5959@douzone.com>
2022-06-30 20:43:10 +0900

    commit

--- BOJ/index.js
+++ BOJ/index.js
@@ -2,6 +2,6 @@
 let input = require('fs').readFileSync('input.txt').toString().trim().split('\n');
 let ansArr = input.map(iter => iter.split(' '));
 
-for (let i = 0; i < ansArr.length; i++) {
+for (let i = 1; i < ansArr.length; i++) {
     console.log(Number(ansArr[i][0]) + Number(ansArr[i][1]));
 }
