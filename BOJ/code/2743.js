0fecc2d49029e81081766f148fb0f7f299433f84
김동훈A <agmon5959@douzone.com>
2022-07-01 21:47:17 +0900

    BOJ commit

---
+++ BOJ/code/2743.js
@@ -0,0 +1,4 @@
+// 단어길이재기
+let input = require('fs').readFileSync('input.txt').toString().trim().split('\n');
+let a = (input[0]);
+console.log(a.length);
--- BOJ/code/2744.js
+++ BOJ/code/2744.js
@@ -1,3 +1,4 @@
+// 대소문자 바꾸기
 let input = require('fs').readFileSync('input.txt').toString().trim().split('\n');
 let a = (input[0]);
 let str = '';
