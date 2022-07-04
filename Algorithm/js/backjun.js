464b07fdea065e345c4146f612844f5f3ff41b31
김동훈A <agmon5959@douzone.com>
2022-07-04 23:33:05 +0900

    boj commit

--- Algorithm/js/backjun.js
+++ Algorithm/js/backjun.js
@@ -4,3 +4,25 @@ function com(parameter) {
 // 2557 Hello World
 console.log("Hello World!");
 // 
+
+function t1() {
+
+    // 출력은 2줄이다. 1번째 줄엔 제출하지 않은 학생의 출석번호 중 가장 작은 것을 출력하고, 2번째 줄에선 그 다음 출석번호를 출력한다.
+    // const fs = require('fs');
+    // const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+    // const input = fs.readFileSync(filePath).toString().split('\n').pop();
+    let input = "baekjoon";
+    let arr = Array.from({ length: 122 - 97 }).fill(-1);
+
+
+    for (let i = 0; i <= input.length; i++) {
+        debugger
+        if (arr[input[i].charCodeAt() - 97] !== -1) {
+            continue;
+        }
+        arr[input[i].charCodeAt() - 97] = i;
+    }
+
+    console.log(arr);
+}
+console.log(t1());
--- BOJ/index.js
+++ BOJ/index.js
@@ -1,20 +1,17 @@
 // 출력은 2줄이다. 1번째 줄엔 제출하지 않은 학생의 출석번호 중 가장 작은 것을 출력하고, 2번째 줄에선 그 다음 출석번호를 출력한다.
-const fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-const input = fs.readFileSync(filePath).toString().split('\n');
-const grade = {
-    "A+": 4.3,
-    "A0": 4.0.toFixed(1),
-    "A-": 3.7,
-    "B+": 3.3,
-    "B0": 3.0.toFixed(1),
-    "B-": 2.7,
-    "C+": 2.3,
-    "C0": 2.0.toFixed(1),
-    "C-": 1.7,
-    "D+": 1.3,
-    "D0": 1.0.toFixed(1),
-    "D-": 0.7,
-    "F": 0.0.toFixed(1),
+let input = require('fs').readFileSync('input.txt').toString().trim().split('\n').pop();
+let arr = Array.from({ length: 122 - 97 + 1 }).fill(-1);
+
+
+for (let i = 0; i < input.length; i++){
+    if (arr[input[i].charCodeAt() - 97] !== -1) {
+        continue;
+    }
+    arr[input[i].charCodeAt() - 97] = i;
 }
-console.log(grade[input]);
+// let result = [];
+// for (let i = 97; i <= 122; i++) {
+//     result.push(input.indexOf(String.fromCharCode(i)));
+// }
+
+console.log(arr.join(" "));
