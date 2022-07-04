8825881109088d4a9a36cac27e1a33b2f8da74e7
김동훈A <agmon5959@douzone.com>
2022-07-04 22:53:32 +0900

    daily boj

--- BOJ/code/새싹/basic.js
+++ BOJ/code/새싹/basic.js
@@ -220,3 +220,27 @@ function _2738() {
         process.exit();
     });
 }
+
+function _2745() {
+    // 출력은 2줄이다. 1번째 줄엔 제출하지 않은 학생의 출석번호 중 가장 작은 것을 출력하고, 2번째 줄에선 그 다음 출석번호를 출력한다.
+    const fs = require('fs');
+    const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+    const input = fs.readFileSync(filePath).toString().split('\n');
+    const grade = {
+        'A+': '4.3',
+        'A0': '4.0',
+        'A-': '3.7',
+        'B+': '3.3',
+        'B0': '3.0',
+        'B-': '2.7',
+        'C+': '2.3',
+        'C0': '2.0',
+        'C-': '1.7',
+        'D+': '1.3',
+        'D0': '1.0',
+        'D-': '0.7',
+        'F': '0.0',
+    }
+    console.log(grade[input]);
+}
+
--- BOJ/index.js
+++ BOJ/index.js
@@ -2,11 +2,19 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = fs.readFileSync(filePath).toString().split('\n');
-let param = input.map(iter => Number(iter)).sort((a, b) => a - b);
-let ans = [];
-for (let i = 0; i < param.length; i++){
-    if (param[i + 1] === undefined) break;
-    if (param[i]+1 !== param[i+1]) {
-        console.log(param[i] + 1);
-    }
+const grade = {
+    "A+": 4.3,
+    "A0": 4.0.toFixed(1),
+    "A-": 3.7,
+    "B+": 3.3,
+    "B0": 3.0.toFixed(1),
+    "B-": 2.7,
+    "C+": 2.3,
+    "C0": 2.0.toFixed(1),
+    "C-": 1.7,
+    "D+": 1.3,
+    "D0": 1.0.toFixed(1),
+    "D-": 0.7,
+    "F": 0.0.toFixed(1),
 }
+console.log(grade[input]);
