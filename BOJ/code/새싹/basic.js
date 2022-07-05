2a7e0a4899edd7c5844a36261a6192846f61f39f
김동훈A <agmon5959@douzone.com>
2022-07-05 22:45:27 +0900

    boj commit

--- BOJ/code/새싹/basic.js
+++ BOJ/code/새싹/basic.js
@@ -244,3 +244,17 @@ function _2745() {
     console.log(grade[input]);
 }
 
+function _9086() {
+    // 출력은 2줄이다. 1번째 줄엔 제출하지 않은 학생의 출석번호 중 가장 작은 것을 출력하고, 2번째 줄에선 그 다음 출석번호를 출력한다.
+    let input = require('fs').readFileSync('input.txt').toString().trim().split('\n');
+    let repeat = input.shift();
+    let param = [...input]
+    for (let i = 0; i < param.length; i++) {
+        if (param[i].length !== 1) {
+            console.log(param[i][0] + param[i][param[i].length - 1]);
+        } else {
+            console.log(param[i] + param[i]);
+        }
+    }
+}
+
--- BOJ/index.js
+++ BOJ/index.js
@@ -1,17 +1,11 @@
 // 출력은 2줄이다. 1번째 줄엔 제출하지 않은 학생의 출석번호 중 가장 작은 것을 출력하고, 2번째 줄에선 그 다음 출석번호를 출력한다.
-let input = require('fs').readFileSync('input.txt').toString().trim().split('\n').pop();
-let arr = Array.from({ length: 122 - 97 + 1 }).fill(-1);
-
-
-for (let i = 0; i < input.length; i++){
-    if (arr[input[i].charCodeAt() - 97] !== -1) {
-        continue;
+let input = require('fs').readFileSync('input.txt').toString().trim().split('\n');
+let repeat = input.shift();
+let param =  [...input]
+for (let i = 0; i < param.length; i++){
+    if (param[i].length !== 1) {
+        console.log(param[i][0] + param[i][param[i].length-1]);
+    } else {
+        console.log(param[i] + param[i]);
     }
-    arr[input[i].charCodeAt() - 97] = i;
 }
-// let result = [];
-// for (let i = 97; i <= 122; i++) {
-//     result.push(input.indexOf(String.fromCharCode(i)));
-// }
-
-console.log(arr.join(" "));
