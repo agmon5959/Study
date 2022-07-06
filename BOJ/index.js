b43e8c691e2d3a2dff8fc7f6846aa6040e02bd15
김동훈A <agmon5959@douzone.com>
2022-07-06 23:23:11 +0900

    boj commit

--- BOJ/index.js
+++ BOJ/index.js
@@ -1,11 +1,14 @@
-// 출력은 2줄이다. 1번째 줄엔 제출하지 않은 학생의 출석번호 중 가장 작은 것을 출력하고, 2번째 줄에선 그 다음 출석번호를 출력한다.
 let input = require('fs').readFileSync('input.txt').toString().trim().split('\n');
-let repeat = input.shift();
-let param =  [...input]
-for (let i = 0; i < param.length; i++){
-    if (param[i].length !== 1) {
-        console.log(param[i][0] + param[i][param[i].length-1]);
-    } else {
-        console.log(param[i] + param[i]);
-    }
+let num = input.pop();
+let ans=0;
+for (let i = 0; i < num.length; i++){
+    ans += Math.pow((num[i]), 5);
 }
+// let dfs = (d,p) => {
+//     if (d === 5) {
+//         return
+//     } else {
+//         dfs(d, p * p);
+//     }
+// }
+console.log(ans);
