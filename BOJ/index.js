e994e8c9c84af7b6dc6dd20e8378dc20a7ba3b87
김동훈A <agmon5959@douzone.com>
2022-07-14 19:19:44 +0900

    daily boj

--- BOJ/index.js
+++ BOJ/index.js
@@ -1,23 +1,50 @@
 let input = require('fs').readFileSync('input.txt').toString().trim().split('\n');
-let seg = input[1].split(' ').map(e => Number(e));
-let stack = [];
-let ansArr = Array.from({ length: seg.length }).fill(-1);
-let obj = {}
+input.shift();
+let calc = [...input.shift()];
+let pNum = [...input];
+const len = calc.length;
+const numStack = [];
+let cnt = 0;
+let obj = {};
 
-seg.forEach((iter) => {
-    if (obj[iter] !== undefined) {
-        obj[iter] += 1;
+function checkType(value) {
+    if (value.charCodeAt() <= 90 && value.charCodeAt() >= 65) {
+        return true;
     } else {
-        obj[iter] = 1;
+        return false;
+    }
+}
+
+
+calc.forEach((iter) => {
+    if (checkType(iter)) {
+        obj[iter] = pNum[cnt];
+        if (pNum[cnt + 1] !== undefined) {
+            cnt++;
+        }
+
     }
 })
 
-for (let i = 0; i < Number(input[0]); i++) {
-    while (stack.length && obj[seg[i]] > obj[seg[stack[stack.length - 1]]]) {
-        ansArr[stack.pop()] = seg[i];
-    }
-    stack.push(i)
-}
 
-console.log(ansArr.join(' '));
+for (let i = 0; i < len; i++) {
+    let fv;
+    let tv;
+
+    if (checkType(calc[i])) { // 알파벳
+        numStack.push(obj[calc[i]]);
+    } else {
+        fv = Number(numStack.pop());
+        tv = Number(numStack.pop());
+        if (calc[i] === "+") {
+            numStack.push(fv + tv);
+        } else if (calc[i] === "-") {
+            numStack.push(tv - fv);
+        } else if (calc[i] === "*") {
+            numStack.push(fv * tv);
+        } else {
+            numStack.push(tv / fv);
+        }
+    }
+}
 
