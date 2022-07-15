2f6fbc72ceb09cd26f006f6b4ef7d67dfa6fe968
김동훈A <agmon5959@douzone.com>
2022-07-15 22:06:02 +0900

    daily boj

--- BOJ/index.js
+++ BOJ/index.js
@@ -1,50 +1,10 @@
 let input = require('fs').readFileSync('input.txt').toString().trim().split('\n');
-input.shift();
-let calc = [...input.shift()];
-let pNum = [...input];
-const len = calc.length;
-const numStack = [];
-let cnt = 0;
-let obj = {};
+let ans = Array.from({ length: 26 }).fill(0);
 
-function checkType(value) {
-    if (value.charCodeAt() <= 90 && value.charCodeAt() >= 65) {
-        return true;
-    } else {
-        return false;
-    }
-}
+// 97 ~ 122
+[...input.pop()].forEach((iter => {
+    ans[iter.charCodeAt() - 97] += 1;
+    // console.log(iter.charCodeAt() - 97)
+}))
 
-
-calc.forEach((iter) => {
-    if (checkType(iter)) {
-        obj[iter] = pNum[cnt];
-        if (pNum[cnt + 1] !== undefined) {
-            cnt++;
-        }
-
-    }
-})
-
-
-for (let i = 0; i < len; i++) {
-    let fv;
-    let tv;
-
-    if (checkType(calc[i])) { // 알파벳
-        numStack.push(obj[calc[i]]);
-    } else {
-        fv = Number(numStack.pop());
-        tv = Number(numStack.pop());
-        if (calc[i] === "+") {
-            numStack.push(fv + tv);
-        } else if (calc[i] === "-") {
-            numStack.push(tv - fv);
-        } else if (calc[i] === "*") {
-            numStack.push(fv * tv);
-        } else {
-            numStack.push(tv / fv);
-        }
-    }
-}
-
+console.log(ans.join(' '));
