4d006e0a1a58c896e789fc2d3c21c7fca5dec41a
김동훈A <agmon5959@douzone.com>
2022-07-15 22:06:13 +0900

    daily boj

--- Algorithm/js/backjun.js
+++ Algorithm/js/backjun.js
@@ -63,4 +63,22 @@ function 오등큰수() {
 
     console.log(ansArr);
 }
-console.log(오등큰수());
+
+
+function _10808() {
+    let input = require('fs').readFileSync('input.txt').toString().trim().split('\n');
+    let ans = Array.from({ length: 26 }).fill(0);
+
+    // 97 ~ 122
+    [...input.pop()].forEach((iter => {
+        ans[iter.charCodeAt() - 97] += 1;
+        // console.log(iter.charCodeAt() - 97)
+    }))
+
+    console.log(ans.join(' '));
+
+}
+
+function _1918() {
+
+}
