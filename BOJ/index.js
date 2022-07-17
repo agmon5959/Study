12e8b8d70e0bce2617a959c228a7d56804c48b3b
김동훈A <agmon5959@douzone.com>
2022-07-17 17:28:12 +0900

    daily boj

--- BOJ/index.js
+++ BOJ/index.js
@@ -1,16 +1,27 @@
 const input = require('fs').readFileSync('input.txt').toString().split('\n')
+let returnArr = [];
+let param = [...input.pop()];
+for (let x of param) {
+    let value = x.charCodeAt();
+    if (value >= 97 && value <= 122) {
+        value += 13;
+        if (value > 122) {
+            value = value - 26;
+        }
 
-const t = input.filter(str => str.length < 1)
+        returnArr.push(String.fromCharCode(value));
+    } else if (value >= 65 && value <= 90) {
+        value += 13;
+        if (value > 90) {
+            value = value - 26;
+        }
 
-if (t.length) {
-    input.splice(input.indexOf(t[0]), 1)
+        returnArr.push(String.fromCharCode(value));
+    } else if (value === 32) {
+        returnArr.push(' ');
+    } else {
+        returnArr.push(x);
+    }
+
 }
-
-input.forEach(str => {
-    const lower = str.length - str.replace(/[a-z]/g, '').length
-    const upper = str.length - str.replace(/[A-Z]/g, '').length
-    const num = str.length - str.replace(/[0-9]/g, '').length
-    const blank = str.length - str.replace(/\ /g, '').length
-
-    console.log(lower, upper, num, blank)
-})
+console.log(returnArr.join(''));
