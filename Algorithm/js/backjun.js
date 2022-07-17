9e043eb46c2752ae41857ef830a97fc89308e689
김동훈A <agmon5959@douzone.com>
2022-07-17 20:40:20 +0900

    commit

--- Algorithm/js/backjun.js
+++ Algorithm/js/backjun.js
@@ -80,7 +80,52 @@ function _10808() {
 }
 
 function _10820() {
-    
+    const input = require('fs').readFileSync('input.txt').toString().split('\n')
+
+    const t = input.filter(str => str.length < 1)
+
+    if (t.length) {
+        input.splice(input.indexOf(t[0]), 1)
+    }
+
+    input.forEach(str => {
+        const lower = str.length - str.replace(/[a-z]/g, '').length
+        const upper = str.length - str.replace(/[A-Z]/g, '').length
+        const num = str.length - str.replace(/[0-9]/g, '').length
+        const blank = str.length - str.replace(/\ /g, '').length
+
+        console.log(lower, upper, num, blank)
+    })
+}
+
+function _11655() {
+    const input = require('fs').readFileSync('input.txt').toString().split('\n')
+    let returnArr = [];
+    let param = [...input.pop()];
+    for (let x of param) {
+        let value = x.charCodeAt();
+        if (value >= 97 && value <= 122) {
+            value += 13;
+            if (value > 122) {
+                value = value - 26;
+            }
+
+            returnArr.push(String.fromCharCode(value));
+        } else if (value >= 65 && value <= 90) {
+            value += 13;
+            if (value > 90) {
+                value = value - 26;
+            }
+
+            returnArr.push(String.fromCharCode(value));
+        } else if (value === 32) {
+            returnArr.push(' ');
+        } else {
+            returnArr.push(x);
+        }
+
+    }
+    console.log(returnArr.join(''));
 }
 
 function _1918() {
