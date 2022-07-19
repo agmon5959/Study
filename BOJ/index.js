3d20819e6ebf675a6f135aa7d6cfee81c9a3a45c
김동훈A <agmon5959@douzone.com>
2022-07-19 19:38:56 +0900

    daily boj

--- BOJ/index.js
+++ BOJ/index.js
@@ -1,14 +1,21 @@
-const input = require('fs').readFileSync('input.txt').toString().trim().split('\n')
-let param = input.pop();
-let pNum1 = param.split(" ")[0];
-let pNum2 = param.split(" ")[1];
+const input = require('fs').readFileSync('input.txt').toString().trim().split('\n');
+console.log();
 
+const isPrime = (n) => {
+    if (n == 1) {
+        return false;
+    }
 
-function 유클리드호제법(num1, num2) {
-    
-    const gcd = (a, b) => a % b === 0 ? b : gcd(b, a % b);
-    const lcm = (a, b) => a * b / gcd(a, b);
-    return [gcd(num1, num2), lcm(num1, num2)];
+    for (let i = 2; i <= (Math.sqrt(n)); i++) {
+        if (n % i === 0) {
+            return false;
+        }
+    }
+    console.log(n);
 }
 
-console.log(유클리드호제법(Number(pNum1), Number(pNum2)).join('\n') );
+// 추후 풀기
+// 골드바흐의 추측
+input.forEach((iter) => {
+    isPrime(iter)
+})
