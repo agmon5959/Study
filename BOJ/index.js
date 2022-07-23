1f4120450bbf2bfa7a132d03bf401932230a8c5a
김동훈A <agmon5959@douzone.com>
2022-07-23 20:52:14 +0900

    daily boj commit

--- BOJ/index.js
+++ BOJ/index.js
@@ -1,33 +1,21 @@
 const input = require('fs').readFileSync('input.txt').toString().trim().split('\n');
-let param = input.pop().split(' ');
-let value1 = Number(param[0]);
-let value2 = Number(param[1]);
+input.shift();
 
-let valueArr = [];
-
-function getZeroCnt(p) {
-    let tempP = p;
-    let cnt_2 = 0;
-    let cnt_5 = 0;
-
-    while (p >= 2) {
-        cnt_2 += parseInt(p / 2);
-        p /= 2;
-    }
-
-    while (tempP >= 5) {
-        cnt_5 += parseInt(tempP / 5);
-        tempP /= 5;
-    }
-    valueArr.push([cnt_2, cnt_5]);
+function 유클리드호제법(num1, num2) {
+    const gcd = (a, b) => a % b === 0 ? b : gcd(b, a % b);
+    // const lcm = (a, b) => a * b / gcd(a, b);
+    return gcd(num1, num2);
 }
 
-getZeroCnt(value1);
-getZeroCnt(value2);
-getZeroCnt(value1 - value2);
+for (let i = 0; i < input.length; i++){
+    let ans = 0;
+    let numArr = input[i].split(' ');
+    numArr.shift();
+    for (let j = 0; j < numArr.length; j++) {
+        for (let k = j + 1; k < numArr.length; k++) {
+            ans += 유클리드호제법(Number(numArr[j]), Number(numArr[k]));
+        }
+    }
+    console.log(ans);
+}
 
-let firstValue = valueArr[0];
-let secondValue = valueArr[1];
-let thirdValue = valueArr[2];
-
-console.log(Math.min((firstValue[0] - (secondValue[0] + thirdValue[0])), (firstValue[1] - (secondValue[1] + thirdValue[1]))));
